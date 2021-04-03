d2fa8b35bc8ce6b467dc57d80f998fe6d765aa3d
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-04 03:26:20 +0530

    added recipe schema

---
+++ models/Recipe.js
@@ -0,0 +1,44 @@
+import { model, Schema, models } from "mongoose";
+
+const RecipeSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please specify the name of recipe."],
+    },
+    image: {
+      type: String,
+    },
+    prepTime: {
+      type: String,
+      required: [true, "Please specify the prep time of recipe."],
+    },
+    cookTime: {
+      type: String,
+      required: [true, "Please specify the cook time of recipe."],
+    },
+    serves: Number,
+    difficulty: {
+      type: String,
+      required: [true, "Please specify the difficulty of recipe."],
+    },
+    description: {
+      type: String,
+      required: [true, "Please specify the description of recipe."],
+    },
+    ingredients: {
+      type: [Schema.Types.ObjectId],
+      ref: "ingredients",
+    },
+    quantity: [String],
+    preperation: {
+      type: String,
+      required: [true, "Please specify the preperation of recipe."],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default models.Recipe || model("Recipe", RecipeSchema);
