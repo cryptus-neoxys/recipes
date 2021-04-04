7bdf524ebbb2d8b5168bbb752cbc11ea5a4ee1ec
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-04 21:31:55 +0530

    Recipe form boiler

---
+++ components/RecipeForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function RecipeForm({ recipe }) {
+  /* React Hook  From setup ? create : edit */
+
+  return <div>Recipe Form</div>;
+}
---
+++ pages/new.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { RecipeForm } from "../components/RecipeForm";
+
+export default function Home() {
+  return (
+    <div>
+      New Recipe
+      <RecipeForm />
+    </div>
+  );
+}
