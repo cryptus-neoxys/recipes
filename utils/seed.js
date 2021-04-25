22a9626740b8a92286396c855ed7b730cf1fac6d
Aniket Singh Rawat <sprx077@gmail.com>
2021-04-25 17:35:33 +0530

    seeded ingredients

--- utils/seed.js
+++ utils/seed.js
@@ -9,22 +9,29 @@ MongoClient.connect(url, async function (err, db) {
 
   var dbo = db.db("test");
 
-  let rawdata = fs.readFileSync("/home/cryptus/Downloads/train.json");
+  let rawdata = fs.readFileSync(
+    "/home/dikwickley/Downloads/archive/train.json"
+  );
   let data = JSON.parse(rawdata);
 
-  console.log(dbo);
-
-  for (var x = 0; x < 10; x += 1) {
+  for (var x = 0; x < data.length; x += 1) {
     if (data[x].ingredients.length)
       for (var y = 0; y < data[x]?.ingredients.length; x += 1) {
-        ingredients.add({ name: data[x]?.ingredients[y].toLowerCase() });
+        ingredients.add(data[x]?.ingredients[y].toLowerCase());
       }
   }
-  console.log(ingredients);
   try {
-    await dbo.collection("ingredients").insertMany(Array.from(ingredients));
+    ingredients = Array.from(ingredients);
+
+    ingredients = ingredients.map((item) => {
+      return { name: item };
+    });
+
+    console.trace(ingredients);
+
+    await dbo.collection("ingredients").insertMany(ingredients);
   } catch (error) {
-    console.log("hagg diya");
+    console.log(error, "hagg diya");
   }
 });
 
