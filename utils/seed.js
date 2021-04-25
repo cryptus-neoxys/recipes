31f59658071e0f1c8a7c72bdb1ef2fb2ee3d7813
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-04-25 17:22:04 +0530

    seed update

--- utils/seed.js
+++ utils/seed.js
@@ -1,26 +1,31 @@
+const fs = require("fs");
 var MongoClient = require("mongodb").MongoClient;
 var url = process.env.MONGODB_URI;
 
-let ingredients = [];
+let ingredients = new Set();
 
-MongoClient.connect(url, function (err, db) {
+MongoClient.connect(url, async function (err, db) {
   if (err) throw err;
 
   var dbo = db.db("test");
 
-  let rawdata = fs.readFileSync(
-    "/home/dikwickley/Downloads/archive/train.json"
-  );
+  let rawdata = fs.readFileSync("/home/cryptus/Downloads/train.json");
   let data = JSON.parse(rawdata);
 
   console.log(dbo);
 
   for (var x = 0; x < 10; x += 1) {
-    ingredients = [...ingredients, ...data[x]?.ingredients];
+    if (data[x].ingredients.length)
+      for (var y = 0; y < data[x]?.ingredients.length; x += 1) {
+        ingredients.add({ name: data[x]?.ingredients[y].toLowerCase() });
+      }
   }
   console.log(ingredients);
+  try {
+    await dbo.collection("ingredients").insertMany(Array.from(ingredients));
+  } catch (error) {
+    console.log("hagg diya");
+  }
 });
-
-const fs = require("fs");
 
 // console.log(data.slice(0, 10));
