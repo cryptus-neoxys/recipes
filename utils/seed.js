79f258908b1870700a6742110456c735c6794984
Aniket Singh Rawat <sprx077@gmail.com>
2021-04-25 16:58:04 +0530

    update seed

--- utils/seed.js
+++ utils/seed.js
@@ -1,9 +1,26 @@
-import dbConnect from "./dbConnect";
+var MongoClient = require("mongodb").MongoClient;
+var url = process.env.MONGODB_URI;
 
-const seedDb = () => {
-  await dbConnect();
+let ingredients = [];
 
-  async () => {
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
 
+  var dbo = db.db("test");
+
+  let rawdata = fs.readFileSync(
+    "/home/dikwickley/Downloads/archive/train.json"
+  );
+  let data = JSON.parse(rawdata);
+
+  console.log(dbo);
+
+  for (var x = 0; x < 10; x += 1) {
+    ingredients = [...ingredients, ...data[x]?.ingredients];
   }
-}
+  console.log(ingredients);
+});
+
+const fs = require("fs");
+
+// console.log(data.slice(0, 10));
