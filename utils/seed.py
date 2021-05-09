bd20f5ec58c55c33aae37b8a59c3f1511e54e03b
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-05-09 22:38:50 +0530

    🌱 Update seed

--- utils/seed.py
+++ utils/seed.py
@@ -1,7 +1,7 @@
 import json
 import os
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+# from bson.objectid import ObjectId
 client = MongoClient(os.getenv("MONGODB_URI"))
 db = client['test']
 ingredient = db['ingredients']
@@ -10,52 +10,50 @@ f = open("/home/cryptus/Desktop/appetizers1-100.json")
 asd = []
 ingred = []
 jsonA = json.load(f)
-for i in jsonA:
-    for j in i['tags']:
-        if j not in ingred:
-            d = {}
-            d['name'] = j
-            ingred.append(j)
-            asd.append(d)
-# def remDup(asd):
-#     x = []
-#     for i in asd:
-#         if i not in x:
-#             x.append(i)
-#     return x
-ingredient.insert_many(asd)
+# for i in jsonA:
+#     for j in i['tags']:
+#         if j not in ingred:
+#             d = {}
+#             d['name'] = j
+#             ingred.append(j)
+#             asd.append(d)
+# ingredient.insert_many(asd)
 count = 0
-comments = [
-    {
-        'userId': 'sadasdasdasdasdsad',
-        'comment': 'Dark jokes are like food, not everyone gets it'
-    }
-]
-print(ingred)
+# comments = [
+#     {
+#         'userId': 'sadasdasdasdasdsad',
+#         'comment': 'Dark jokes are like food, not everyone gets it'
+#     }
+# ]
+# print(ingred)
 for i in jsonA:
     if 'video' in i['video'] and 'cook:' in i and 'prep:' in i and 'total:' in i:
         name = ' '.join(i['video'].split('/')[-2].split('-')).title()
+        i['image'] = i['images'][0]
         i['name'] = name
-        i['cook'] = i['cook:']
-        i['prep'] = i['prep:']
+        i['cookTime'] = i['cook:']
+        i['prepTime'] = i['prep:']
         try:
-            i['additional'] = i['additional:']
+            del i['images']
+        except:
+            pass
+        try:
             del i['additional:']
         except:
             pass
-        i['total'] = i['total:']
+        try:
+            del i['total:']
+        except:
+            pass
+        try:
+            del i['Yield:']
+        except:
+            pass
         i['serves'] = i['Servings:']
         del i['cook:']
         del i['prep:']
-        del i['total:']
         del i['Servings:']
-        i['author'] = 'Sample User ObjectId'
-        i['likes'] = ['User1Id', 'User1Id', 'User1Id', 'User1Id', 'User1Id']
-        i['comments'] = comments
-        try:
-            del i['Yield']
-        except:
-            pass
+        i['author'] = None
         for j in range(len(i['tags'])):
             i['tags'][j] = ingredient.find_one({'name': i['tags'][j]})['_id']
         recipe.insert_one(i)
