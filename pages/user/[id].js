4d64d43a2b4f5b4ca3675c44f4b0b93ffdc472cc
Aniket Singh Rawat <sprx077@gmail.com>
2021-05-14 12:17:46 +0530

    added server side props for users

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -4,66 +4,50 @@ import { RecipeCard } from "@components/RecipeCard";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/client";
 
-export default () => {
-  const router = useRouter();
-  const [session, loading] = useSession();
-  const [recipes, setRecipes] = useState(false);
-  const [user, setUser] = useState(false);
-
-  const { id } = router.query;
-
-  const fetchRecipes = async () => {
-    try {
-      let user = await fetch(`/api/user/${id}`, {
-        method: "GET",
-      });
-      user = await user.json();
-      console.log(user);
-      if (user.success === true) {
-        setUser(user.data);
-      }
-      // console.log(user);
-
-      if (user) {
-        let data = await fetch(`/api/recipe/user/${user.data._id}`, {
-          method: "GET",
-        });
-        data = await data.json();
-        console.log(data);
-        setRecipes(data.data);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  // const fetchRecipes = async () =>
-
-  useEffect(() => {
-    fetchRecipes();
-  }, [router.query.id]);
+export default ({ user, recipes }) => {
+  console.log(user);
+  console.log(recipes);
 
   return (
-    <Layout>
+    <Layout title={user.name}>
       <div className="w-2/3 mx-auto">
-        {user ? (
-          <div>
-            <div className="text-4xl">{user.name}</div>
+        <div>
+          <div className="text-4xl">{user.name}</div>
 
-            <div className="mt-4 text-xl">
-              Recipes created by {user.email.split("@")[0]}:
-            </div>
-            <div className="flex flex-row flex-wrap mb-6">
-              {recipes &&
-                recipes.map((recipe, key) => {
-                  return <RecipeCard recipe={recipe} key={key} />;
-                })}
-            </div>
+          <div className="mt-4 text-xl">
+            Recipes created by {user.email.split("@")[0]}:
           </div>
-        ) : (
-          <div className="text-4xl"> User not found :( </div>
-        )}
+          <div className="flex flex-row flex-wrap mb-6">
+            {recipes &&
+              recipes.map((recipe, key) => {
+                return <RecipeCard recipe={recipe} key={key} />;
+              })}
+          </div>
+        </div>
       </div>
     </Layout>
   );
 };
+
+export async function getStaticPaths() {
+  const res = await fetch("http://localhost:3000/api/user");
+  const data = await res.json();
+  const users = data.data;
+  const paths = users.map((user) => ({
+    params: { id: user.email.split("@")[0] },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  let res = await fetch(`http://localhost:3000/api/user/${params.id}`);
+  let data = await res.json();
+  const user = data.data;
+
+  res = await fetch(`http://localhost:3000/api/recipe/user/${user._id}`);
+  data = await res.json();
+  const recipes = data.data;
+
+  return { props: { user, recipes } };
+}
