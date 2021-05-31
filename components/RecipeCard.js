10d61dafe78d66900123e486352687d5f3367531
Aniket Singh Rawat <sprx077@gmail.com>
2021-05-31 20:51:27 +0530

    better UI for bookmarks

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import React from "react";
+import { signIn, useSession } from "next-auth/client";
 
 const Tag = ({ ingredient }) => {
   return (
@@ -11,6 +12,7 @@ const Tag = ({ ingredient }) => {
 
 export const RecipeCard = ({ recipe, id, bookmark, email }) => {
   const router = useRouter();
+  const [session, loading] = useSession();
 
   const handleBookmark = async () => {
     console.log(email, bookmark, recipe._id);
@@ -23,8 +25,9 @@ export const RecipeCard = ({ recipe, id, bookmark, email }) => {
         method: "POST",
       });
       console.log(res);
+      router.push(`/user/${session.user.email.split("@")[0]}`);
     } else {
-      alert("Login to Bookmark");
+      signIn("google");
     }
   };
 
@@ -34,14 +37,15 @@ export const RecipeCard = ({ recipe, id, bookmark, email }) => {
         className="h-[250px] w-[250px]  bg-cover bg-center bg-no-repeat  rounded-t-3xl"
         style={{
           backgroundImage: `url(${recipe.image || "/fallback.png"})`,
-        }}></div>
-      <div className=" flex flex-col p-3 text-white bg-black">
+        }}
+      ></div>
+      <div className="flex flex-col p-3 text-white bg-black ">
         <div className="flex flex-row items-center justify-between">
           <div className="font-bold text-1xl w-[150px] break-words">
             {recipe.name}
           </div>
 
-          <div className=" text-xs">{recipe.cookTime}</div>
+          <div className="text-xs ">{recipe.cookTime}</div>
         </div>
 
         <div className="flex flex-row flex-wrap mt-4 text-sm text-left text-wrap w-[200px]">
@@ -50,22 +54,28 @@ export const RecipeCard = ({ recipe, id, bookmark, email }) => {
             return <Tag ingredient={ingredient.name} key={key} />;
           })}
         </div>
-        <img
-          id={id}
-          src="/arrow.png"
-          className="h-[20px] self-end cursor-pointer recipe-select"
-          onClick={() => {
-            router.push(`/recipe/${recipe._id}`);
-          }}
-        />
-        <div>
-          <p onClick={() => handleBookmark()} className="cursor-pointer">
-            {email
-              ? bookmark === -1
-                ? "bookmark"
-                : "Bookmarked"
-              : "Login to Bookmark"}
-          </p>
+
+        <div className="flex flex-row-reverse justify-between mt-5">
+          <img
+            id={id}
+            src="/arrow.png"
+            className="h-[20px] self-end cursor-pointer recipe-select"
+            onClick={() => {
+              router.push(`/recipe/${recipe._id}`);
+            }}
+          />
+
+          <div onClick={() => handleBookmark()} className="cursor-pointer">
+            {email ? (
+              bookmark === -1 ? (
+                <img src="/no-bookmark.png" className="h-[25px]" />
+              ) : (
+                <img src="/bookmark.png" className="h-[25px]" />
+              )
+            ) : (
+              <img src="/no-bookmark.png" className="h-[25px]" />
+            )}
+          </div>
         </div>
       </div>
     </div>
